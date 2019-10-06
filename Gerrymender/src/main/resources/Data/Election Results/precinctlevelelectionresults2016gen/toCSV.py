b2ca602685d166ac4cc5fbb927bfbb141ed0876c
BTMichalowicz <benjaminmichalowicz98@gmail.com>
2019-10-06 16:09:29 -0400

    Started SOME data preprocessing

---
+++ Gerrymender/src/main/resources/Data/Election Results/precinctlevelelectionresults2016gen/toCSV.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+
+from pathlib import Path
+from time import sleep
+import sys
+import os
+import errno
+
+##Benjamin Michalowicz
+##Purpose: To turn election result text files into csv files to be easily separatable in a database setting
+## Java or otherwise
+
+
+
+for file in os.listdir('.'):
+
+    filename = os.fsdecode(file)
+    if filename.endswith(".txt"):
+        ##Parse the text files inside the textfiles from dos.myflorida.com
+        inputfile = open("./"+filename, 'r')
+        outputfile = open("./"+filename.split(".txt")[0]+".csv", 'w')
+        outputfile.write("CountyCode,CountyName,ElectionNumber,ElectionDate,ElectionName,UniquePrecId,PrecPollLoc,TotalRegVot,TotalRep,TotalDem,TotalOther,ContestName,Dist,ContestCode,Candidate-Reten-IssueName-WriteIns-OverUnderVote,CandParty,RegIdNumber,DOECandNumber,VoteTotal\n")
+        line = inputfile.readline()
+        while(line=='\n'):
+            line = inputfile.readline()
+            continue
+        while(line!=''):
+            if line == "\n":
+                while(line =='\n'):
+                    line = inputfile.readline()
+            #Trying to remove empty lines
+            line_arr = line.split('\t')
+            #while ' ' in line_arr:
+            #    line_arr.remove(' ')
+
+            #Trying to remove newline characters
+            for x in line_arr:
+                if '\n' in x:
+                    x.replace('\n', '')
+            #print(line_arr)
+            outputfile.write(line_arr[0])
+
+            for i in range(1, len(line_arr)):
+                outputfile.write("," + line_arr[i])
+           # outputfile.write("\n") 
+
+            line = inputfile.readline()
+
+        outputfile.close()
+        inputfile.close()
+
+
