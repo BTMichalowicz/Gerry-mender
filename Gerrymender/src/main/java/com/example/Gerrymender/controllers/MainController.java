ac2f40f33ec21aedd28e6ca7425f591ffbb2174f
samzhang <8812992>
2019-10-08 18:51:47 -0400

    1. freeze the home page map 2. show different colors for each states

--- Gerrymender/src/main/java/com/example/Gerrymender/controllers/MainController.java
+++ Gerrymender/src/main/java/com/example/Gerrymender/controllers/MainController.java
@@ -3,14 +3,13 @@ package com.example.Gerrymender.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class MainController {
 
     @RequestMapping("/")
     public String welcome(){
-        return "GerryMenderTest";
+        return "Test";
     }
 
 }
