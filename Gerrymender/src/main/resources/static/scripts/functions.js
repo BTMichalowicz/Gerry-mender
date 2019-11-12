d31fb844b84b3d60f8b1735e9fa578e74020ed29
samzhang3442 <jing.zhang.6@stonybrook.edu>
2019-11-12 15:30:50 -0500

    add district color map to show different color for each district

--- Gerrymender/src/main/resources/static/scripts/functions.js
+++ Gerrymender/src/main/resources/static/scripts/functions.js
@@ -104,3 +104,41 @@
     function show_value(x, id){
     document.getElementById(id).innerHTML=x;
     }
+
+    var district_color = new Map();
+    district_color.set(1, '#ff6d3a');
+    district_color.set(2, '#1531ff');
+    district_color.set(3, '#B22222');
+    district_color.set(4, '#1E90FF');
+    district_color.set(5, '#FFD700');
+    district_color.set(6, '#ADFF2F');
+    district_color.set(7, '#4B0082');
+    district_color.set(8, '#CD853F');
+    district_color.set(9, '#40E0D0');
+    district_color.set(10, '#FF6347');
+    district_color.set(11, '#6A5ACD');
+    district_color.set(12, '#87CEEB');
+    district_color.set(13, '#B0C4DE');
+    district_color.set(14, '#FF0000');
+    district_color.set(15, '#50b2ff');
+    district_color.set(16, '#6effa1');
+    district_color.set(17, '#20f8ff');
+    district_color.set(18, '#ffa71d');
+    district_color.set(19, '#ff2fab');
+    district_color.set(20, '#faffbd');
+    district_color.set(21, '#cbcdff');
+    district_color.set(22, '#ffb7b9');
+    district_color.set(23, '#adff8f');
+    district_color.set(24, '#eaff78');
+    district_color.set(25, '#6fff6b');
+    district_color.set(26, '#e584ff');
+    district_color.set(27, '#51caff');
+    district_color.set(28, '#ffe46e');
+    district_color.set(29, '#57b8ff');
+    district_color.set(30, '#ff71c7');
+    district_color.set(31, '#90ff83');
+    district_color.set(32, '#ffd48f');
+    district_color.set(33, '#58c3ff');
+    district_color.set(34, '#ff9de8');
+    district_color.set(35, '#a7ff74');
+    district_color.set(36, '#ff726f');
