bf747208b8097c1d784be512320a5b59bfed2cfe
samzhang3442 <jing.zhang.6@stonybrook.edu>
2019-11-19 01:29:19 -0500

    1. update cluster class field

--- Gerrymender/src/main/java/com/example/Gerrymender/model/Cluster.java
+++ Gerrymender/src/main/java/com/example/Gerrymender/model/Cluster.java
@@ -14,59 +14,59 @@ public class  Cluster implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name="ClusterID")
-    private long id;
+    @Column
+    private long clusterid;
 
 
     @Id
-    @Column(name="stateName") //Clusters for specific state
-    private String stateName;
-    private ArrayList<Precinct> precincts;
-    private ArrayList<Edge> edges;
+    @Column
+    private String statename;
+//    private ArrayList<Precinct> precincts;
+//    private ArrayList<Edge> edges;
 
-    @Column(name="ClusterPop")
+    @Column
     private long population;
 
-    @Column(name="totalPop")
-    private long totalPop;
-    @Column(name="Pol_Part")
-    private Pol_part party;
-    @Column(name="whitePop")
-    private long white_pop;
-    @Column(name="africanAmericanPop")
-    private long africanAmerican_pop;
-    @Column(name="hispanicPop")
-    private long hispanic_pop;
-    @Column(name="asianPop")
-    private long asian_pop;
-    @Column(name="nativePop")
-    private long nativeAmerican_pop;
+    @Column
+    private long totalpop;
+    @Column
+    private String party;
+    @Column
+    private long whitepop;
+    @Column
+    private long africanamericanpop;
+    @Column
+    private long hispanicpop;
+    @Column
+    private long asianpop;
+    @Column
+    private long nativeamericanpop;
 
 
-    public ArrayList<Edge> getEdges() {
-        return edges;
-    }
-
-    public void setEdges(ArrayList<Edge> edges) {
-        assert(edges!=null);
-        this.edges = edges;
-    }
-
-    public ArrayList<Precinct> getPrecincts() {
-        return precincts;
-    }
-
-    public void setPrecincts(ArrayList<Precinct> precincts) {
-        assert(precincts!=null);
-        this.precincts = precincts;
-    }
+//    public ArrayList<Edge> getEdges() {
+//        return edges;
+//    }
+//
+//    public void setEdges(ArrayList<Edge> edges) {
+//        assert(edges!=null);
+//        this.edges = edges;
+//    }
+//
+//    public ArrayList<Precinct> getPrecincts() {
+//        return precincts;
+//    }
+//
+//    public void setPrecincts(ArrayList<Precinct> precincts) {
+//        assert(precincts!=null);
+//        this.precincts = precincts;
+//    }
 
     public String getStateName() {
-        return stateName;
+        return statename;
     }
 
     public void setStateName(String stateName) {
-        this.stateName = stateName;
+        this.statename = stateName;
     }
 
     public long getPopulation() {
@@ -79,11 +79,11 @@ public class  Cluster implements Serializable {
     }
 
     public long getId() {
-        return id;
+        return clusterid;
     }
 
     public void setId(long id) {
-        this.id = id;
+        this.clusterid = id;
     }
 
 
@@ -103,43 +103,43 @@ public class  Cluster implements Serializable {
 
 
     public long getNativeAmerican_pop() {
-        return nativeAmerican_pop;
+        return nativeamericanpop;
     }
 
     public void setNativeAmerican_pop(long nativeAmerican_pop) {
-        this.nativeAmerican_pop = nativeAmerican_pop;
+        this.nativeamericanpop = nativeAmerican_pop;
     }
 
     public long getAsian_pop() {
-        return asian_pop;
+        return asianpop;
     }
 
     public void setAsian_pop(long asian_pop) {
-        this.asian_pop = asian_pop;
+        this.asianpop = asian_pop;
     }
 
     public long getHispanic_pop() {
-        return hispanic_pop;
+        return hispanicpop;
     }
 
     public void setHispanic_pop(long hispanic_pop) {
-        this.hispanic_pop = hispanic_pop;
+        this.hispanicpop = hispanic_pop;
     }
 
     public long getAfricanAmerican_pop() {
-        return africanAmerican_pop;
+        return africanamericanpop;
     }
 
     public void setAfricanAmerican_pop(long africanAmerican_pop) {
-        this.africanAmerican_pop = africanAmerican_pop;
+        this.africanamericanpop = africanAmerican_pop;
     }
 
     public long getWhite_pop() {
-        return white_pop;
+        return whitepop;
     }
 
     public void setWhite_pop(long white_pop) {
-        this.white_pop = white_pop;
+        this.whitepop = white_pop;
     }
 }
 
