66a08ffe1bd0cb5dfe0ebe845a47831403fc3520
vquintana23 <veronica.quintana@stonybrook.edu>
2019-11-20 12:20:34 -0500

    NC ONLY: redoing menu tab. currently help tab displays the help menu.

--- Gerrymender/src/main/resources/static/scripts/functions.js
+++ Gerrymender/src/main/resources/static/scripts/functions.js
@@ -40,7 +40,7 @@
     }
 
     function moveZoomControl() {
-        if (sliderState === 1) { //Open slider
+        if (sliderState === 1) { 
             container = map.zoomControl.getContainer(),
                 containerTop = 350 + 'px';
             container.style.position = 'absolute';
@@ -55,7 +55,7 @@
     }
 
     function toggleInfoSlider( feature ) {
-        if (infoStat === feature.properties.DISTRICT) { //Open slider
+        if (infoStat === feature.properties.DISTRICT) { 
             document.getElementById("slide-info").style.width = "0";
             infoStat = "null";
 
@@ -66,6 +66,27 @@
 
         }
     }
+
+    function openTab(evt, tabName) {
+        // Declare all variables
+        var i, tabcontent, tablinks;
+      
+        // Get all elements with class="tabcontent" and hide them
+        tabcontent = document.getElementsByClassName("tabcontent");
+        for (i = 0; i < tabcontent.length; i++) {
+          tabcontent[i].style.display = "none";
+        }
+      
+        // Get all elements with class="tablinks" and remove the class "active"
+        tablinks = document.getElementsByClassName("tablinks");
+        for (i = 0; i < tablinks.length; i++) {
+          tablinks[i].className = tablinks[i].className.replace(" active", "");
+        }
+      
+        // Show the current tab, and add an "active" class to the button that opened the tab
+        document.getElementById(tabName).style.display = "block";
+        evt.currentTarget.className += " active";
+      }
 
     function closeInfo() {
         document.getElementById("slide-info").style.width = "0";
