68db218aebec65ec800a2c3dbb7183d72ab8001d
ipeitzsch <irpeitzsch@gmail.com>
2019-12-13 16:13:30 -0500

    Added majority demographic to phase0 return

--- Gerrymender/src/main/java/com/example/Gerrymender/Abstractions/VotingBlocInfo.java
+++ Gerrymender/src/main/java/com/example/Gerrymender/Abstractions/VotingBlocInfo.java
@@ -1,16 +1,19 @@
 package com.example.Gerrymender.Abstractions;
 
 import com.example.Gerrymender.model.Pol_part;
+import com.example.Gerrymender.model.Race;
 
 public class VotingBlocInfo {
     public String precinctId;
     public Pol_part party;
     public int totalVotes;
     public int partyVotes;
-    public VotingBlocInfo(String precinctID, Pol_part party, int totalVotes, int partyVotes) {
+    public Race majorityRace;
+    public VotingBlocInfo(String precinctID, Pol_part party, int totalVotes, int partyVotes, Race majorityRace) {
         this.party = party;
         this.partyVotes = partyVotes;
         this.precinctId = precinctID;
         this.totalVotes = totalVotes;
+        this.majorityRace = majorityRace;
     }
 }
